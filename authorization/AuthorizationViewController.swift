a60c76ef8e4ec8c6cc742a1a6cf31b7254127ec1
Zalman Zoteev <sz.zoteev@gmail.com>
2023-03-31 15:17:07 +0300

    create new class

---
+++ authorization/AuthorizationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  authorization
+//
+//  Created by Zalman Zoteev on 31/03/2023.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+
+    @IBOutlet var passwordOutlet: UITextField!
+    @IBOutlet var loginOutlet: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logInTapped(_ sender: Any) {
+    }
+    
+    @IBAction func remaindLoginTapped(_ sender: Any) {
+    }
+    @IBAction func remaindPasswordTapped(_ sender: Any) {
+    }
+}
+
